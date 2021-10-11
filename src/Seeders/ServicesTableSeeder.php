408d719537e1ee5f6fef1c7eccc20974cd709f39
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-10-11 22:49:23 +0300

    Create seeder files

---
+++ src/Seeders/ServicesTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Rutatiina\Services\Seeders;
+
+use Illuminate\Database\Seeder;
+use Rutatiina\Admin\Models\Service;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Service::create([
+            'code' => 'financial-accounting',
+            'name' => 'Financial Accounting',
+            'type' => 'app',
+            'description' => 'Financial Accounting application',
+        ]);
+
+        Service::create([
+            'code' => 'hrm',
+            'name' => 'HRM',
+            'type' => 'app',
+            'description' => 'Human Resource Management',
+        ]);
+
+        Service::create([
+            'code' => 'crbt',
+            'name' => 'CRBT - Caller Ring Back Tunes',
+            'type' => 'app',
+            'description' => 'CRBT - Caller Ring Back Tunes',
+        ]);
+
+        Service::create([
+            'code' => 'workshop',
+            'name' => 'Workshop',
+            'type' => 'app',
+            'description' => 'Workshop',
+        ]);
+
+    }
+}
